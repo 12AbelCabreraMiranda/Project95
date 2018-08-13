f652cd64316bc1cf302d7577e2b88cc45a504810
AbelCab93 <cabrerabel@hotmail.com>
2018-08-12 23:39:07 -0600

    separamos avance a otro archivo

---
+++ avance.php
@@ -0,0 +1,18 @@
+                <!-- CUERPO DE Avance-->
+                <div class="col-md-4 avance">                    
+                    <a href="#"> <img class="img-responsive img-circle" id="imagenPerfil" style="height:90px; width:auto" src="img/abel.jpg"> </a>                    
+                    
+                    <p class="text-center" id="tituloAvance">Nivel de Avance</p>
+                    <p style="color:white" >Fruta Dulce</p>
+                    <div class="progress">                                                
+                        <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 95%">                            
+                            <span class="sr-only">60% Complete</span>95%
+                        </div>
+                    </div>
+                    <p style="color:white">Fruta Ácido</p>
+                    <div class="progress">                                                
+                        <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 15%">                            
+                            <span class="sr-only">60% Complete</span>15%
+                        </div>
+                    </div>
+                </div>
