bcf42e781bbd5a128ee4ba4df94092490d9f5034
AbelCab93 <cabrerabel@hotmail.com>
2018-09-15 18:39:32 -0600

    audio agregado en modulo examen cuerpo humano

---
+++ examen_cuerpoHumano/audios.js
@@ -0,0 +1,26 @@
+// AUDIO VOCALES
+
+var voz1= new Audio();
+voz1.src = "../audio/cuerpo_humano/cualeseldedo.mp3";
+
+var voz2= new Audio();
+voz2.src = "../audio/cuerpo_humano/cualeselojo.mp3";
+
+var voz3= new Audio();
+voz3.src = "../audio/cuerpo_humano/cualeslacabeza.mp3";
+
+var voz4= new Audio();
+voz4.src = "../audio/cuerpo_humano/cualeslosdientes.mp3";
+
+var voz5= new Audio();
+voz5.src = "../audio/cuerpo_humano/cualeslamano.mp3";
+
+// validacion 
+
+var voz6= new Audio();
+voz6.src = "../audio/vocales/respuestacorrecta.mp3";
+
+var voz7= new Audio();
+voz7.src = "../audio/vocales/eleccionequivocada.mp3";
+
+
