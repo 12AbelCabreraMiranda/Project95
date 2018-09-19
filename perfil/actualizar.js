4693b7ead77c92a1239fe7ab0c7c3440735cf20f
AbelCab93 <cabrerabel@hotmail.com>
2018-09-19 15:43:31 -0600

    desarrollando disenio en Perfil completado extrallendo datos y mostrandolos en los input de acuerdo al usuario logeado

--- perfil/actualizar.js
+++ perfil/actualizar.js
@@ -1,5 +1,5 @@
+// -------------------- FUNCION EDITAR NOMBRE ----------------------
 function habilitarNombre(){
-    //alert("habilitado");
     var habil= document.getElementById("nombre");
     habil.disabled=false;     
     $('#botonGuardarNombre').show(); 
@@ -10,8 +10,37 @@ function habilitarNombre(){
     habil.disabled=true;     
     $('#botonGuardarApellido').hide(); 
     $('#botonCancelarApellido').hide();
+
+    //DESACTIVAR ESTABLECIMIENTO
+    var habil= document.getElementById("establecimiento");
+    habil.disabled=true;     
+    $('#botonGuardarEstablecimiento').hide(); 
+    $('#botonCancelarEstablecimiento').hide();
+    
+    //DESACTIVAR PROFESION
+    var habil= document.getElementById("profesion");
+    habil.disabled=true;     
+    $('#botonGuardarProfesion').hide(); 
+    $('#botonCancelarProfesion').hide(); 
+}
+// BOTON guardar cambios
+function ActulizarNombre(){
+    var habil= document.getElementById("nombre");
+    habil.disabled=true;     
+    $('#botonGuardarNombre').hide(); 
+    $('#botonCancelarNombre').hide();    
+}
+// BOTON cancelar
+function CancelarNombre(){
+    var habil= document.getElementById("nombre");
+    habil.disabled=true;     
+    $('#botonGuardarNombre').hide(); 
+    $('#botonCancelarNombre').hide();    
 }
 
+
+
+// -------------------- FUNCION EDITAR APELLIDO ----------------------
 function habilitarApellido(){
     var habil= document.getElementById("apellido");
     habil.disabled=false;     
@@ -24,4 +53,117 @@ function habilitarApellido(){
     $('#botonGuardarNombre').hide(); 
     $('#botonCancelarNombre').hide();
 
+    //DESACTIVAR ESTABLECIMIENTO
+    var habil= document.getElementById("establecimiento");
+    habil.disabled=true;     
+    $('#botonGuardarEstablecimiento').hide(); 
+    $('#botonCancelarEstablecimiento').hide();
+
+     //DESACTIVAR PROFESION
+     var habil= document.getElementById("profesion");
+     habil.disabled=true;     
+     $('#botonGuardarProfesion').hide(); 
+     $('#botonCancelarProfesion').hide(); 
+}
+
+// BOTON guardar cambios
+function ActulizarApellido(){
+    var habil= document.getElementById("apellido");
+    habil.disabled=true;     
+    $('#botonGuardarApellido').hide(); 
+    $('#botonCancelarApellido').hide();    
+}
+// BOTON cancelar
+function CancelarApellido(){
+    var habil= document.getElementById("apellido");
+    habil.disabled=true;     
+    $('#botonGuardarApellido').hide(); 
+    $('#botonCancelarApellido').hide();    
+}
+
+
+
+// -------------------- FUNCION EDITAR ESTABLECIMIENTO ----------------------
+function habilitarEstablecimiento(){
+    var habil= document.getElementById("establecimiento");
+    habil.disabled=false;     
+    $('#botonGuardarEstablecimiento').show(); 
+    $('#botonCancelarEstablecimiento').show(); 
+
+    //DESACTIVAR NOMBRE
+    var habil= document.getElementById("nombre");
+    habil.disabled=true;     
+    $('#botonGuardarNombre').hide(); 
+    $('#botonCancelarNombre').hide();
+
+    //DESACTIVAR APELLIDO
+    var habil= document.getElementById("apellido");
+    habil.disabled=true;     
+    $('#botonGuardarApellido').hide(); 
+    $('#botonCancelarApellido').hide();
+
+     //DESACTIVAR PROFESION
+     var habil= document.getElementById("profesion");
+     habil.disabled=true;     
+     $('#botonGuardarProfesion').hide(); 
+     $('#botonCancelarProfesion').hide(); 
+}
+
+// BOTON guardar cambios
+function ActulizarEstablecimiento(){
+    var habil= document.getElementById("establecimiento");
+    habil.disabled=true;     
+    $('#botonGuardarEstablecimiento').hide(); 
+    $('#botonCancelarEstablecimiento').hide();    
+}
+// BOTON cancelar
+function CancelarEstablecimiento(){
+    var habil= document.getElementById("establecimiento");
+    habil.disabled=true;     
+    $('#botonGuardarEstablecimiento').hide(); 
+    $('#botonCancelarEstablecimiento').hide();    
+}
+
+
+
+
+// -------------------- FUNCION EDITAR PROFESIÓN ----------------------
+function habilitarProfesion(){
+    var habil= document.getElementById("profesion");
+    habil.disabled=false;     
+    $('#botonGuardarProfesion').show(); 
+    $('#botonCancelarProfesion').show(); 
+
+    //DESACTIVAR NOMBRE
+    var habil= document.getElementById("nombre");
+    habil.disabled=true;     
+    $('#botonGuardarNombre').hide(); 
+    $('#botonCancelarNombre').hide();
+
+    //DESACTIVAR APELLIDO
+    var habil= document.getElementById("apellido");
+    habil.disabled=true;     
+    $('#botonGuardarApellido').hide(); 
+    $('#botonCancelarApellido').hide();
+
+    //DESACTIVAR establecimiento
+    var habil= document.getElementById("establecimiento");
+    habil.disabled=true;     
+    $('#botonGuardarEstablecimiento').hide(); 
+    $('#botonCancelarEstablecimiento').hide(); 
+}
+
+// BOTON guardar cambios
+function ActulizarProfesion(){
+    var habil= document.getElementById("profesion");
+    habil.disabled=true;     
+    $('#botonGuardarProfesion').hide(); 
+    $('#botonCancelarProfesion').hide();    
+}
+// BOTON cancelar
+function CancelarProfesion(){
+    var habil= document.getElementById("profesion");
+    habil.disabled=true;     
+    $('#botonGuardarProfesion').hide(); 
+    $('#botonCancelarProfesion').hide();    
 }
--- perfil/miPerfil.php
+++ perfil/miPerfil.php
@@ -1,3 +1,29 @@
+<?php
+    require('..//bd/conexion.php');
+    session_start();
+    $usuMaestro = $_SESSION['u_usuario'];
+
+    $id;
+    $query1 = ("SELECT id_maestroU FROM usuario where nom_usuario='$usuMaestro'");
+    $result1 = $conexion->query($query1);
+    if($row = $result1->fetch_assoc()){      
+        $id =$row['id_maestroU'];
+     }
+
+    $nombre;
+    $apellido;
+    $establecimiento;
+    $profesion;
+    $query = "SELECT nombre, apellido,establecimiento,profesion from maestro WHERE idmaestro='$id' ";
+    $resultado = $conexion->query($query);
+    if($row = $resultado->fetch_assoc()){      
+        $nombre =$row['nombre'];
+        $apellido =$row['apellido'];
+        $establecimiento =$row['establecimiento'];
+        $profesion =$row['profesion'];
+     }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,8 +38,7 @@
 </head>
 <body>
     <!--PERMITE REDIRECCIONARLO AL LOGIN SI NO HAY SESION INICIADA -->
-    <?php // AGREGARLO EN LAS DEMAS PAGINAS PARA QUE LOS QUE ESTEN CON SESION INICIADO PUEDAN ACCEDER ELSE NOT ACCESS
-        session_start();
+    <?php // AGREGARLO EN LAS DEMAS PAGINAS PARA QUE LOS QUE ESTEN CON SESION INICIADO PUEDAN ACCEDER ELSE NOT ACCESS        
         if(isset($_SESSION['u_usuario'])){
         }else{
             header("Location: login/login.php");
@@ -24,32 +49,63 @@
     <div class="container"><br>
 
     <!--EDICION DE DATOS -->
-    <div class="col-md-6 col-lg-offset-3">
-        <center><h1>Actulizacion de Datos</h1></center><br>
+    <div class="col-md-4 col-lg-offset-4">
+        <center><h1>Actulización de Datos</h1></center><br>
         <!--NOMBRE -->
         <form action="miPerfil.php" class="form-horizontal" method="post" id="form_nombre" enctype="multipart/form-data" ><!--Permite dar saltos de espacios entre filas -->
             <div class="form-group"><!--Agrupacion -->
-                <label class="control-label col-md-2">NOMBRE:</label><!--control-label: permite tenerlos a l 100% -->
-                <div class="input-group">                    
-                    <input class="form-control" nome="nombre" id="nombre" type="text" disabled >
+                <label class="control-label ">NOMBRE:</label>
+                <div class="input-group">                       
+                    <div class="input-group-addon"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></div>             
+                    <input class="form-control" nome="nombre" id="nombre" type="text" disabled value="<?php  echo $row['nombre']; ?>" >
                     <div class="input-group-addon" style="cursor:pointer" onclick="habilitarNombre()">
-                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></div>
+                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                    </div>
                 </div><br>
                 <input type="button" class="btn btn-default botonGuardar" id="botonGuardarNombre" onclick="ActulizarNombre()" value="Guardar Cambios">
                 <input type="button" class="btn btn-default botonCancelar"id="botonCancelarNombre" onclick="CancelarNombre()" value="Cancelar">
             </div>
         </form>
         <!--APELLIDO -->
-        <form action="miPerfil.php" class="form-horizontal" method="post" id="form_apellido" enctype="multipart/form-data" ><!--Permite dar saltos de espacios entre filas -->
+        <form action="miPerfil.php" class="form-horizontal" method="post" id="form_apellido" enctype="multipart/form-data" style="margin-top:-20px"><!--Permite dar saltos de espacios entre filas -->
             <div class="form-group"><!--Agrupacion -->
-                <label class="control-label col-md-2">APELLIDO:</label><!--control-label: permite tenerlos a l 100% -->
-                <div class="input-group">                    
-                    <input class="form-control" nome="apellido" id="apellido" type="text" disabled >
+                <label class="control-label ">APELLIDO:</label>
+                <div class="input-group">    
+                    <div class="input-group-addon"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></div>                 
+                    <input class="form-control" nome="apellido" id="apellido" type="text" disabled value="<?php  echo $row['apellido']; ?>">
                     <div class="input-group-addon" style="cursor:pointer" onclick="habilitarApellido()">
                     <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></div>
                 </div><br>
                 <input type="button" class="btn btn-default botonGuardar" id="botonGuardarApellido" onclick="ActulizarApellido()" value="Guardar Cambios">
-                <input type="button" class="btn btn-default botonCancelar"id="botonCancelarApellido" onclick="CancelarNombre()" value="Cancelar">
+                <input type="button" class="btn btn-default botonCancelar"id="botonCancelarApellido" onclick="CancelarApellido()" value="Cancelar">
+            </div>
+        </form>
+        <!--ESTABLECIMIENTO -->
+        <form action="miPerfil.php" class="form-horizontal" method="post" id="form_establecimiento" enctype="multipart/form-data" style="margin-top:-20px"><!--Permite dar saltos de espacios entre filas -->
+            <div class="form-group"><!--Agrupacion -->
+                <label class="control-label ">ESTABLECIMIENTO:</label>
+                <div class="input-group">    
+                    <div class="input-group-addon"><span class="glyphicon glyphicon-th" aria-hidden="true"></span></div>                 
+                    <input class="form-control" nome="establecimiento" id="establecimiento" type="text" disabled value="<?php  echo $row['establecimiento']; ?>">
+                    <div class="input-group-addon" style="cursor:pointer" onclick="habilitarEstablecimiento()">
+                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></div>
+                </div><br>
+                <input type="button" class="btn btn-default botonGuardar" id="botonGuardarEstablecimiento" onclick="ActulizarEstablecimiento()" value="Guardar Cambios">
+                <input type="button" class="btn btn-default botonCancelar"id="botonCancelarEstablecimiento" onclick="CancelarEstablecimiento()" value="Cancelar">
+            </div>
+        </form>
+        <!--PROFESION -->
+        <form action="miPerfil.php" class="form-horizontal" method="post" id="form_profesion" enctype="multipart/form-data" style="margin-top:-20px"><!--Permite dar saltos de espacios entre filas -->
+            <div class="form-group"><!--Agrupacion -->
+                <label class="control-label ">PROFESION:</label>
+                <div class="input-group">    
+                    <div class="input-group-addon"><span class="glyphicon glyphicon-education" aria-hidden="true"></span> </div>                 
+                    <input class="form-control" nome="profesion" id="profesion" type="text" disabled value="<?php  echo $row['profesion']; ?>">
+                    <div class="input-group-addon" style="cursor:pointer" onclick="habilitarProfesion()">
+                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></div>
+                </div><br>
+                <input type="button" class="btn btn-default botonGuardar" id="botonGuardarProfesion" onclick="ActulizarProfesion()" value="Guardar Cambios">
+                <input type="button" class="btn btn-default botonCancelar"id="botonCancelarProfesion" onclick="CancelarProfesion()" value="Cancelar">
             </div>
         </form>
 
